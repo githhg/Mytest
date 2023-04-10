605f736c0d8489b5a6332befdf5c9ac31abbe1f5
LY <7807045@qq.com>
2023-04-10 15:26:38 +0800

    第一次提交

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+     public static void main(String []args){
+        System.out.println("hello world line1");
+	System.out.println("hello world line2");
+	System.out.println("hello world line3");
+	System.out.println("hello world line4");
+	System.out.println("hello world line5");
+     }
+}
