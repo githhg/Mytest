ab3fb85c281e3959c9d6a936b5590aecad79041f
LY <7807045@qq.com>
2023-04-10 17:01:00 +0800

    aa

--- World.java
+++ World.java
@@ -1,7 +1,6 @@
 public class World{
    public static void main（String [] args）{
       System.out.println("world line 1“);
-      System.out.println("world line 2“);
       System.out.println("world line 3“);
       System.out.println("world line 4“);
       System.out.println("world line 5 edit by hot_fix“);
